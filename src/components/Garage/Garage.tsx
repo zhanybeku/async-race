09ac607aa9f6d77c0ae2e8541381d49a7febfc79
Zhanybek Bekbolat <zhanybek.bekbolat@gmail.com>
2026-06-27 17:52:43 +0500

    Add separate car buttons and adjust lost progress

--- src/components/Garage/Garage.tsx
+++ src/components/Garage/Garage.tsx
@@ -1,172 +1,277 @@
-import { useState, useEffect } from 'react';
-import './garage.css';
-import CarIcon from '../../assets/garage/car-top-view.svg?react';
+import { useState, useEffect } from "react";
+import "./garage.css";
+import CarIcon from "../../assets/garage/car-top-view.svg?react";
 
-import Button from '@mui/material/Button';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import ReplayIcon from '@mui/icons-material/Replay';
-import TextField from '@mui/material/TextField';
-import DeleteIcon from '@mui/icons-material/Delete';
+import Button from "@mui/material/Button";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import ReplayIcon from "@mui/icons-material/Replay";
+import TextField from "@mui/material/TextField";
+import DeleteIcon from "@mui/icons-material/Delete";
 
-import { createCar, deleteCar, getCars, updateCar } from '../../api/api';
+import {
+  createCar,
+  deleteCar,
+  driveEngine,
+  getCars,
+  startStopEngine,
+  updateCar,
+} from "../../api/api";
 
-import type { CreateCar } from '../../ts/interfaces';
+import type { CreateCar } from "../../ts/interfaces";
 
 interface Car {
-    id: number;
-    name: string;
-    color: string;
+  id: number;
+  name: string;
+  color: string;
 }
 
 const Garage = () => {
-    // The cars state
-    const [cars, setCars] = useState<Car[]>([]);
+  // The cars state
+  const [cars, setCars] = useState<Car[]>([]);
 
-    // The new car's states
-    const [newCarColor, setNewCarColor] = useState('#000000');
-    const [newCarName, setNewCarName] = useState<string>('');
+  // The new car's states
+  const [newCarColor, setNewCarColor] = useState("#000000");
+  const [newCarName, setNewCarName] = useState<string>("");
 
-    // The states for the car that's being edited or deleted
-    const [editId, setEditId] = useState<number | null>(null);
-    const [editCarName, setEditCarName] = useState<string>('');
-    const [editCarColor, setEditCarColor] = useState<string>('#000000');
-    const [deleteId, setDeleteId] = useState<number | null>(null);
+  // The states for the car that's being edited or deleted
+  const [editId, setEditId] = useState<number | null>(null);
+  const [editCarName, setEditCarName] = useState<string>("");
+  const [editCarColor, setEditCarColor] = useState<string>("#000000");
+  const [deleteId, setDeleteId] = useState<number | null>(null);
 
-    useEffect(() => {
-        getCars().then(setCars);
-    }, []);
+  // States for engine and driving
+  const [racingIds, setRacingIds] = useState<Set<number>>(new Set());
+  //
 
-    const onCarClick = (id: number) => {
-        setEditId(id);
-        setEditCarName(cars.find(car => car.id === id)?.name || '');
-        setEditCarColor(cars.find(car => car.id === id)?.color || '#000000');
-    }
+  useEffect(() => {
+    getCars().then(setCars);
+  }, []);
 
-    const onSaveCar = (id: number) => {
-        if (!editCarName.trim()) return;
-        const updatedCar: CreateCar = { name: editCarName, color: editCarColor };
-        updateCar(id, updatedCar).then(() => getCars().then(setCars));
-        setEditId(null);
-    }
+  // Car editing and creation
+  const onCarClick = (id: number) => {
+    setEditId(id);
+    setEditCarName(cars.find((car) => car.id === id)?.name || "");
+    setEditCarColor(cars.find((car) => car.id === id)?.color || "#000000");
+  };
 
-    const onCreateCar = () => {
-        if (!newCarName.trim()) return;
-        const newCar: CreateCar = { name: newCarName, color: newCarColor };
-        createCar(newCar).then(() => getCars().then(setCars));
-        setNewCarName('');
-        setNewCarColor('#000000');
-    }
+  const onSaveCar = (id: number) => {
+    if (!editCarName.trim()) return;
+    const updatedCar: CreateCar = { name: editCarName, color: editCarColor };
+    updateCar(id, updatedCar).then(() => getCars().then(setCars));
+    setEditId(null);
+  };
 
-    const onSetToDelete = (id: number) => {
-        setEditId(null);
-        setEditCarName('');
-        setEditCarColor('#000000');
-        setDeleteId(id);
-    }
+  const onCreateCar = () => {
+    if (!newCarName.trim()) return;
+    const newCar: CreateCar = { name: newCarName, color: newCarColor };
+    createCar(newCar).then(() => getCars().then(setCars));
+    setNewCarName("");
+    setNewCarColor("#000000");
+  };
 
-    const onDeleteCar = (id: number) => {
-        deleteCar(id).then(() => getCars().then(setCars));
-    }
+  const onSetToDelete = (id: number) => {
+    setEditId(null);
+    setEditCarName("");
+    setEditCarColor("#000000");
+    setDeleteId(id);
+  };
 
-    return (
-        <div>
-            <div className='buttons-container'>
-                <div className='race-buttons'>
-                    <Button variant="contained" startIcon={<PlayArrowIcon />} sx={{ backgroundColor: 'var(--red)' }}>
-                        START
+  const onDeleteCar = (id: number) => {
+    deleteCar(id).then(() => getCars().then(setCars));
+  };
+
+  // Engine functions
+  const onDriveCar = async (id: number) => {
+    console.log(id);
+  };
+
+  const onResetCar = (id: number) => {
+    console.log(id);
+  };
+
+  return (
+    <div>
+      <div className="buttons-container">
+        <div className="race-buttons">
+          <Button
+            variant="contained"
+            startIcon={<PlayArrowIcon />}
+            sx={{ backgroundColor: "var(--red)" }}
+          >
+            START
+          </Button>
+
+          <Button
+            variant="contained"
+            startIcon={<ReplayIcon />}
+            sx={{ backgroundColor: "var(--blue)" }}
+          >
+            RESET
+          </Button>
+
+          <span className="hover-text">
+            Click a car's name to edit or delete it!
+          </span>
+        </div>
+
+        <div className="car-creation-buttons">
+          <TextField
+            label="Car name"
+            variant="outlined"
+            size="small"
+            sx={{
+              "& .MuiInputBase-input": {
+                backgroundColor: "white",
+                borderRadius: "4px",
+              },
+              "& .MuiInputLabel-root.MuiInputLabel-shrink": {
+                backgroundColor: "white",
+                padding: "0 7px",
+                marginLeft: "-4px",
+                borderRadius: "4px",
+              },
+            }}
+            value={newCarName}
+            onChange={(e) => setNewCarName(e.target.value)}
+          />
+
+          <input
+            type="color"
+            className="color-picker"
+            value={newCarColor}
+            onChange={(e) => setNewCarColor(e.target.value)}
+          />
+
+          <Button
+            variant="contained"
+            sx={{ backgroundColor: "var(--green)" }}
+            onClick={onCreateCar}
+          >
+            CREATE
+          </Button>
+        </div>
+      </div>
+
+      <table className="racetrack-table">
+        <tbody>
+          {cars.map((car, index) => (
+            <tr key={car.id}>
+              <td className="start-zone">
+                <div className="start-zone-div">
+                  <div className="individual-car-buttons">
+                    <Button
+                      onClick={() => onDriveCar(car.id)}
+                      disabled={racingIds.has(car.id)}
+                      size="small"
+                    >
+                      <PlayArrowIcon fontSize="small" />
                     </Button>
 
-                    <Button variant="contained" startIcon={<ReplayIcon />} sx={{ backgroundColor: 'var(--blue)' }}>
-                        RESET
+                    <Button onClick={() => onResetCar(car.id)} size="small">
+                      <ReplayIcon fontSize="small" />
                     </Button>
+                  </div>
 
-                    <span className='hover-text'>Click a car's name to edit or delete it!</span>
+                  <CarIcon
+                    className="carIcon"
+                    width={40}
+                    height={40}
+                    fill={car.color}
+                    stroke="white"
+                    strokeWidth={0.5}
+                  />
                 </div>
+              </td>
+              {index === 0 && (
+                <td className="start-line" rowSpan={cars.length}>
+                  START
+                </td>
+              )}
 
-                <div className='car-creation-buttons'>
-                    <TextField label="Car name" variant="outlined" size="small"
-                        sx={{
-                            '& .MuiInputBase-input': { backgroundColor: 'white', borderRadius: '4px' },
-                            '& .MuiInputLabel-root.MuiInputLabel-shrink': {
-                                backgroundColor: 'white',
-                                padding: '0 7px',
-                                marginLeft: '-4px',
-                                borderRadius: '4px',
-                            }
-                        }}
-                        value={newCarName}
-                        onChange={(e) => setNewCarName(e.target.value)}
+              <td className="track">
+                {editId === car.id ? (
+                  <div className="edit-car-container">
+                    <TextField
+                      variant="outlined"
+                      size="small"
+                      value={editCarName}
+                      onChange={(e) => setEditCarName(e.target.value)}
+                      sx={{
+                        "& .MuiInputBase-input": {
+                          backgroundColor: "white",
+                          borderRadius: "4px",
+                        },
+                      }}
                     />
 
-                    <input type="color" className='color-picker' value={newCarColor} onChange={(e) => setNewCarColor(e.target.value)} />
+                    <input
+                      type="color"
+                      className="color-picker"
+                      value={editCarColor}
+                      onChange={(e) => setEditCarColor(e.target.value)}
+                    />
 
-                    <Button variant="contained" sx={{ backgroundColor: 'var(--green)' }} onClick={onCreateCar}>
-                        CREATE
+                    <Button
+                      variant="contained"
+                      sx={{ backgroundColor: "var(--green)" }}
+                      onClick={() => onSaveCar(car.id)}
+                    >
+                      SAVE
+                    </Button>
+                    <Button
+                      variant="contained"
+                      sx={{ backgroundColor: "var(--blue)" }}
+                      onClick={() => setEditId(null)}
+                    >
+                      CANCEL
                     </Button>
 
-                </div>
-            </div>
+                    <Button
+                      variant="contained"
+                      sx={{ backgroundColor: "var(--red)" }}
+                      onClick={() => onSetToDelete(car.id)}
+                    >
+                      <DeleteIcon />
+                    </Button>
+                  </div>
+                ) : deleteId === car.id ? (
+                  <div className="delete-car-container">
+                    <span>Are you sure you want to delete this car?</span>
+                    <Button
+                      variant="contained"
+                      sx={{ backgroundColor: "var(--red)" }}
+                      onClick={() => onDeleteCar(car.id)}
+                    >
+                      YES
+                    </Button>
+                    <Button
+                      variant="contained"
+                      sx={{ backgroundColor: "var(--blue)" }}
+                      onClick={() => setDeleteId(null)}
+                    >
+                      NO
+                    </Button>
+                  </div>
+                ) : (
+                  <span className="car-name" onClick={() => onCarClick(car.id)}>
+                    {car.name}
+                  </span>
+                )}
+              </td>
 
-            <table className='racetrack-table'>
-                <tbody>
-                    {cars.map((car, index) => (
-                        <tr key={car.id}>
-                            <td className='start-zone'>
-                                {/* {car.name} */}
-                                <CarIcon
-                                    className="carIcon"
-                                    width={40} height={40}
-                                    fill={car.color}
-                                    stroke="white"
-                                    strokeWidth={0.5}
-                                />
-                            </td>
-                            {index === 0 && (
-                                <td className='start-line' rowSpan={cars.length}>START</td>
-                            )}
+              {index === 0 && (
+                <td className="finish-line" rowSpan={cars.length}>
+                  FINISH
+                </td>
+              )}
 
-                            <td className='track'>
-                                {editId === car.id ? (
-                                    <div className='edit-car-container'>
-                                        <TextField variant="outlined" size="small"
-                                            value={editCarName}
-                                            onChange={(e) => setEditCarName(e.target.value)}
-                                            sx={{
-                                                '& .MuiInputBase-input': { backgroundColor: 'white', borderRadius: '4px' },
-                                            }}
-                                        />
+              <td className="finish-zone"></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-                                        <input type="color" className='color-picker' value={editCarColor} onChange={(e) => setEditCarColor(e.target.value)} />
-
-                                        <Button variant="contained" sx={{ backgroundColor: 'var(--green)' }} onClick={() => onSaveCar(car.id)}>SAVE</Button>
-                                        <Button variant="contained" sx={{ backgroundColor: 'var(--blue)' }} onClick={() => setEditId(null)}>CANCEL</Button>
-
-                                        <Button variant="contained" sx={{ backgroundColor: 'var(--red)' }} onClick={() => onSetToDelete(car.id)}><DeleteIcon /></Button>
-                                    </div>
-                                ) : deleteId === car.id ? (
-                                    <div className='delete-car-container'>
-                                        <span>Are you sure you want to delete this car?</span>
-                                        <Button variant="contained" sx={{ backgroundColor: 'var(--red)' }} onClick={() => onDeleteCar(car.id)}>YES</Button>
-                                        <Button variant="contained" sx={{ backgroundColor: 'var(--blue)' }} onClick={() => setDeleteId(null)}>NO</Button>
-                                    </div>
-                                ) :
-                                    (
-                                        <span className='car-name' onClick={() => onCarClick(car.id)}>{car.name}</span>
-                                    )}
-                            </td>
-
-                            {index === 0 && (
-                                <td className='finish-line' rowSpan={cars.length}>FINISH</td>
-                            )}
-
-                            <td className='finish-zone'></td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-
-        </div>
-    )
-}
-
-export default Garage
+export default Garage;
